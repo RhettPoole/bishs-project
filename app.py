7cb4536f1ee9e5c402151065532f42bd6508bb34
RhettPoole <rhettpoole20@byui.edu>
2025-02-23 23:55:39 -0700

    app.py notes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# https://bishs-services-f22217090ef6.herokuapp.com/
+# To view this app: https://bishs-services-f22217090ef6.herokuapp.com/
 # ** Disclaimer - This application is for experimentation and demonstration /n
 # purposes only. It is not intended for official use. Although labeled as a /n
 # project for Bish's, it is solely a showcase of my development skills and /n
